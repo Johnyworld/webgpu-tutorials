496d1a8630fb2209dfa9de390e67b74089797492
johnyworld <johnyworld@naver.com>
2023-12-29 16:41:16 +0900

    webpack 설치 & index js 파일 반영

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        type: 'javascript/esm',
+      },
+    ],
+  },
+};
